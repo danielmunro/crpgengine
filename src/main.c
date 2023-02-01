4bf0d3fdc7b01e38a9d79489046dc05c3983e529
Dan Munro <dan@danmunro.com>
2023-01-31 18:09:38 -0800

    refactor

--- src/main.c
+++ src/main.c
@@ -1,30 +1,15 @@
-/*******************************************************************************************
-*
-*   raylib [textures] example - Sprite animation
-*
-*   Example originally created with raylib 1.3, last time updated with raylib 1.3
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2014-2023 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "header.h"
 
 int main(void)
 {
-    // Initialization
     const int screenWidth = 800;
     const int screenHeight = 450;
     SetTargetFPS(60);
 
-    InitWindow(screenWidth, screenHeight, "raylib [texture] example - sprite anim");
+    InitWindow(screenWidth, screenHeight, "cjrpgengine");
 
     struct Game *g = createGameInstance();
-
-    // Main game loop
+    
     while (!WindowShouldClose())
     {
         if (IsKeyDown(KEY_RIGHT)) move(g->player->sprite, DIRECTION_RIGHT);
