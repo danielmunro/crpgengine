7d4797057247e3ebf5bae7a56c759b93678cfb9a
Dan Munro <dan@danmunro.com>
2023-02-04 10:25:12 -0800

    remove dummy code

--- src/tilemap.c
+++ src/tilemap.c
@@ -10,21 +10,6 @@ typedef struct Tilemap {
     Object *objects[MAX_OBJECTS];
 } Tilemap;
 
-Tilemap *createTestTilemap() {
-    Tilemap *t = malloc(sizeof(Tilemap));
-    Vector2D size = {16, 16 };
-    t->size = size;
-    t->source = LoadTexture("resources/overworld.png");
-
-    for (int y = 0; y < MAX_LAYER_SIZE / 4; y++) {
-        for (int x = 0; x < MAX_LAYER_SIZE / 4; x++) {
-            t->layers[0][y][x] = 0;
-        }
-    }
-
-    return t;
-}
-
 Vector2D getTileFromIndex(Tilemap *t, int index) {
     int width = t->source.width / t->size.x;
     int x, y;
