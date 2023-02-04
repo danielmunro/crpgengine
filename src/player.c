07c579d727780a4fa3ae4ca705521a375952b52b
Dan Munro <dan@danmunro.com>
2023-02-04 06:48:00 -0800

    refactor movement to be more free-form

--- src/player.c
+++ src/player.c
@@ -1,17 +1,32 @@
 typedef struct Player {
     struct Sprite *sprite;
-    int x;
-    int y;
+    Vector2 pos;
     int isMoving;
-    long duration;
+    struct timeval lastMovement;
 } Player;
 
 Player *createTestPlayer() {
     Player *player = malloc(sizeof(Player));
     player->sprite = createTestHumanoid();
-    player->x = 10;
-    player->y = 10;
+    player->pos.x = 10;
+    player->pos.y = 10;
     player->isMoving = 0;
-    player->duration = 0;
+    gettimeofday(&player->lastMovement, NULL);
     return player;
+}
+
+void movePlayer(Player *p, int direction) {
+    p->isMoving = 1;
+    p->sprite->direction = direction;
+    if (direction == DIRECTION_UP) {
+        p->pos.y -= 0.1f;
+    } else if (direction == DIRECTION_DOWN) {
+        p->pos.y += 0.1f;
+    }
+    if (direction == DIRECTION_LEFT) {
+        p->pos.x -= 0.1f;
+    } else if (direction == DIRECTION_RIGHT) {
+        p->pos.x += 0.1f;
+    }
+    gettimeofday(&p->lastMovement, NULL);
 }
--- src/scene.c
+++ src/scene.c
@@ -18,27 +18,12 @@ const SceneType sceneTypes[] = {
     {SCENE_TYPE_MENU, "menu"},
 };
 
-void movePlayer(Scene *s, int direction) {
-    s->player->sprite->direction = direction;
-    if (direction == DIRECTION_UP) {
-        s->player->y = s->player->y - 1;
-    } else if (direction == DIRECTION_DOWN) {
-        s->player->y = s->player->y + 1;
-    }
-    if (direction == DIRECTION_LEFT) {
-        s->player->x = s->player->x - 1;
-    } else if (direction == DIRECTION_RIGHT) {
-        s->player->x = s->player->x + 1;
-    }
-    s->player->isMoving = 1;
-}
-
 void checkInput(Scene *s) {
     if (s->type == SCENE_TYPE_FREE_MOVE) {
-        if (IsKeyDown(KEY_RIGHT)) movePlayer(s, DIRECTION_RIGHT);
-        else if (IsKeyDown(KEY_LEFT)) movePlayer(s, DIRECTION_LEFT);
-        else if (IsKeyDown(KEY_UP)) movePlayer(s, DIRECTION_UP);
-        else if (IsKeyDown(KEY_DOWN)) movePlayer(s, DIRECTION_DOWN);
+        if (IsKeyDown(KEY_RIGHT)) movePlayer(s->player, DIRECTION_RIGHT);
+        else if (IsKeyDown(KEY_LEFT)) movePlayer(s->player, DIRECTION_LEFT);
+        else if (IsKeyDown(KEY_UP)) movePlayer(s->player, DIRECTION_UP);
+        else if (IsKeyDown(KEY_DOWN)) movePlayer(s->player, DIRECTION_DOWN);
         else s->player->isMoving = 0;
     }
 }
@@ -46,8 +31,8 @@ void checkInput(Scene *s) {
 void drawLayer(Scene *s, Layer layer) {
     int tiles_x = SCREEN_WIDTH / s->tilemap->size.x + 1;
     int tiles_y = SCREEN_HEIGHT / s->tilemap->size.y + 2;
-    int start_y = s->player->y - (tiles_y / 2);
-    int start_x = s->player->x - (tiles_x / 2);
+    int start_y = (int)s->player->pos.y - (tiles_y / 2);
+    int start_x = (int)s->player->pos.x - (tiles_x / 2);
     Vector2D sz = s->tilemap->size;
     int i = 0;
     while (i < MAX_LAYER_COUNT && i < s->layers) {
@@ -68,9 +53,13 @@ void drawLayer(Scene *s, Layer layer) {
                         (float) sz.x,
                         (float) sz.y
                 };
+                int xint = (int)s->player->pos.x;
+                float xdiff = s->player->pos.x - (float)xint;
+                int yint = (int)s->player->pos.y;
+                float ydiff = s->player->pos.y - (float)yint;
                 Vector2 pos = {
-                        (float) (sz.x * x),
-                        (float) (sz.y * y)
+                        (float) (sz.x * x) - (xdiff * (float)sz.x),
+                        (float) (sz.y * y) - (ydiff * (float)sz.y)
                 };
                 DrawTextureRec(
                         s->tilemap->source,
