802a42cbaed33ce33417444ed58675ce3a6d8c90
Dan Munro <dan@danmunro.com>
2023-05-05 06:06:06 -0700

    rearrange

--- src/test.c
+++ src/test.c
@@ -1,14 +1,20 @@
 #include "header.h"
 #include "tap.h"
 
-void pathCatDealsWithMultipleSlashTest() {
+void strToIntTest() {
+    ok(strToInt("123042") == 123042, "strToInt works as expected");
+}
+
+void pathCatTest() {
     // given
     char *path1 = "/path/to/";
-    char *path2 = "/dir";
+    char *path2 = "dir";
+    char *path3 = "./nested";
+    char *expected = "/path/to/dir/./nested";
 
     // when
-    char *result = pathCat(path1, path2);
-    char *expected = "/path/to/dir";
+    char *part = pathCat(path1, path2);
+    char *result = pathCat(part, path3);
 
     // then
     is(result, expected, "paths should match");
@@ -27,29 +33,23 @@ void pathCatDealsWithLeadingSlashTest() {
     is(result, expected, "paths should match");
 }
 
-void pathCatTest() {
+void pathCatDealsWithMultipleSlashTest() {
     // given
     char *path1 = "/path/to/";
-    char *path2 = "dir";
-    char *path3 = "./nested";
-    char *expected = "/path/to/dir/./nested";
+    char *path2 = "/dir";
 
     // when
-    char *part = pathCat(path1, path2);
-    char *result = pathCat(part, path3);
+    char *result = pathCat(path1, path2);
+    char *expected = "/path/to/dir";
 
     // then
     is(result, expected, "paths should match");
 }
 
-void strToIntTest() {
-    ok(strToInt("123042") == 123042, "strToInt works as expected");
-}
-
 int main() {
     plan(4);
+    strToIntTest();
     pathCatTest();
     pathCatDealsWithLeadingSlashTest();
     pathCatDealsWithMultipleSlashTest();
-    strToIntTest();
 }
