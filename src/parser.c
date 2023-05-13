4ecf96c4c1d1d52bb4ac46ee30747d4c7d2ac5fa
Dan Munro <dan@danmunro.com>
2023-05-13 07:18:16 -0700

    mv

--- src/parser.c
+++ src/parser.c
@@ -1,3 +1,49 @@
+void clean(char *value) {
+    while (true) {
+        if (value[0] != ' ') {
+            break;
+        }
+        memmove(&value[0], &value[1], strlen(value));
+    }
+    if (value[0] == '"') {
+        memmove(&value[0], &value[1], strlen(value));
+        unsigned long l = strlen(value);
+        if (value[l - 1] == '"') {
+            memmove(&value[l - 1], &value[l], 1);
+        }
+    }
+}
+
+void parseKV(char *data, char *result[2]) {
+    result[0] = strtok(data, ":");
+    result[1] = strtok(NULL, "\r\n");
+    clean(result[0]);
+    if (result[1] != NULL) {
+        clean(result[1]);
+    }
+}
+
+int parseKVPairs(char *data, char *result[255]) {
+    char *rows[255];
+    char *row = strtok(data, "\r\n");
+    int r = 0;
+    while(row != NULL) {
+        rows[r] = row;
+        row = strtok(NULL, "\r\n");
+        r++;
+    }
+    int j = 0;
+    for (int i = 0; i < r; i++) {
+        char *kv[2];
+        parseKV(rows[i], kv);
+        result[j] = kv[0];
+        result[j + 1] = kv[1];
+        printf("kv: %s, %s\n", kv[0], kv[1]);
+        j += 2;
+    }
+    return j;
+}
+
 int isControl(char *key) {
     if (strcmp(key, "when") == 0) {
         return true;
