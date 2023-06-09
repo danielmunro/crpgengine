71942131d4aca6642b1a5c6f29e4c71b6baf8a3c
Dan Munro <dan@danmunro.com>
2023-06-09 07:25:19 -0700

    add properties to mob

--- src/attributes.c
+++ src/attributes.c
@@ -5,3 +5,13 @@ typedef struct Attributes {
     int wisdom;
     int constitution;
 } Attributes;
+
+Attributes *createAttributes() {
+    Attributes *a = malloc(sizeof(Attributes));
+    a->strength = 0;
+    a->dexterity = 0;
+    a->intelligence = 0;
+    a->wisdom = 0;
+    a->constitution = 0;
+    return a;
+}
--- src/mobile.c
+++ src/mobile.c
@@ -3,6 +3,11 @@ typedef struct Mobile {
     char *id;
     char *name;
     int direction;
+    int gp;
+    int level;
+    int experienceToLevel;
+    int hp;
+    int mana;
     Vector2 position;
     Attributes *attributes;
     Item *items[MAX_ITEMS];
@@ -20,5 +25,11 @@ Mobile *createMobile() {
     for (int i = 0; i < MAX_ANIMATIONS; i++) {
         mobile->animations[i] = NULL;
     }
+    mobile->attributes = createAttributes();
+    mobile->gp = 0;
+    mobile->level = 1;
+    mobile->experienceToLevel = 0;
+    mobile->hp = 1;
+    mobile->mana = 0;
     return mobile;
 }
