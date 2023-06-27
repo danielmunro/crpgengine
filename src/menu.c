44cdbf3089f8f5810590f5eea55b944b3dda26da
Dan Munro <dan@danmunro.com>
2023-06-27 05:45:25 -0700

    rename and mv in game menu to own file

--- src/menu.c
+++ src/menu.c
@@ -17,28 +17,10 @@ void drawPlayer(Player *player) {
             );
 }
 
-void drawInGameMainMenuScreen(Menu *menu, Player *player) {
-    int column1 = (UI_PADDING * 2) + MOB_COLLISION_WIDTH;
-    int column2 = SCREEN_WIDTH - 200;
-    drawText(player->mob->name, (Vector2d){column1, UI_PADDING});
-    char hp[64];
-    sprintf(hp, "hp %d/%d", 20, 20);
-    drawText(hp, (Vector2d){column1, UI_PADDING + line(1)});
-    char mp[64];
-    sprintf(mp, "mp %d/%d", 20, 20);
-    drawText(mp, (Vector2d){column1, UI_PADDING + line(2)});
-
-    int count = sizeof(MainInGameMenuItem) + 1;
-    for (int i = 0; i < count; i++) {
-        drawText(MainInGameMenuItems[i], (Vector2d){column2, UI_PADDING + line(i)});
-    }
-    drawText(">", (Vector2d){column2 - 20, UI_PADDING + line(menu->cursor)});
-}
-
 void drawAllMenus(Player *player, Menu *menu[MAX_MENUS], int menuCount) {
     for (int i = 0; i < menuCount; i++) {
         if (menu[i]->type == MENU_IN_GAME_MAIN) {
-            drawInGameMainMenuScreen(menu[i], player);
+            drawInGameMainMenuScreen(menu[i]->cursor, player);
         }
     }
 }
---
+++ src/menu/main_in_game.c
@@ -0,0 +1,17 @@
+void drawInGameMainMenuScreen(int lineNumber, Player *player) {
+    int column1 = (UI_PADDING * 2) + MOB_COLLISION_WIDTH;
+    int column2 = SCREEN_WIDTH - 200;
+    drawText(player->mob->name, (Vector2d){column1, UI_PADDING});
+    char hp[64];
+    sprintf(hp, "hp %d/%d", 20, 20);
+    drawText(hp, (Vector2d){column1, UI_PADDING + line(1)});
+    char mp[64];
+    sprintf(mp, "mp %d/%d", 20, 20);
+    drawText(mp, (Vector2d){column1, UI_PADDING + line(2)});
+
+    int count = sizeof(MainInGameMenuItem) + 1;
+    for (int i = 0; i < count; i++) {
+        drawText(MainInGameMenuItems[i], (Vector2d){column2, UI_PADDING + line(i)});
+    }
+    drawText(">", (Vector2d){column2 - 20, UI_PADDING + line(lineNumber)});
+}
