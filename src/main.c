cb5656fdc9dd99e7f6fa43e91e6857490d067026
Dan Munro <dan@danmunro.com>
2023-07-19 06:08:31 -0700

    double check

--- src/main.c
+++ src/main.c
@@ -4,10 +4,6 @@ int main(int argc, char *argv[]) {
     SetTraceLogLevel(LOG_WARNING);
     srand(time(NULL));
     RuntimeArgs *r = createRuntimeArgs(argc, argv);
-    if (r->indexDir == NULL) {
-        fprintf(stderr, "required argument -i indexDir missing\n");
-        return 1;
-    }
     initWindow(r->indexDir);
     Game *g = createGame(r);
     validateGameData(g);
