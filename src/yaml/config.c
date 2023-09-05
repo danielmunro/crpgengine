605b05ef465b191c2ce6469d35297a95c51a949f
Dan Munro <dan@danmunro.com>
2023-09-04 22:16:16 -0700

    remove unused property

--- src/yaml/config.c
+++ src/yaml/config.c
@@ -1,13 +1,10 @@
 typedef struct {
     const char *title;
-    const char *font;
 } ConfigData;
 
 static const cyaml_schema_field_t appConfigTopMappingField[] = {
         CYAML_FIELD_STRING_PTR(
                 "title", CYAML_FLAG_POINTER, ConfigData, title, 0, CYAML_UNLIMITED),
-        CYAML_FIELD_STRING_PTR(
-                "font", CYAML_FLAG_POINTER, ConfigData, font, 0, CYAML_UNLIMITED),
         CYAML_FIELD_END
 };
 
