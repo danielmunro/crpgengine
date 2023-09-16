be77f7cec3cccb6bb4f10dbc2c5ce0fda522ab26
Dan Munro <dan@danmunro.com>
2023-09-16 07:25:47 -0700

    don't let vitals go below zero

--- src/mobile.c
+++ src/mobile.c
@@ -213,8 +213,14 @@ void normalizeVitalsForMobile(Mobile *mob) {
     if (mob->hp > calculated.hp) {
         mob->hp = calculated.hp;
     }
+    if (mob->hp < 0) {
+        mob->hp = 0;
+    }
     if (mob->mana > calculated.mana) {
         mob->mana = calculated.mana;
+    }
+    if (mob->mana < 0) {
+        mob->mana = 0;
     }
 }
 
