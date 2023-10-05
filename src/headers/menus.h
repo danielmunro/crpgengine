416f130bc8d7c7c62c60d3affa3399b4a301ad4b
Dan Munro <dan@danmunro.com>
2023-10-05 06:22:56 -0700

    menus

---
+++ src/headers/menus.h
@@ -0,0 +1,21 @@
+#ifndef CJRPGENGINE_MENUS_H
+#define CJRPGENGINE_MENUS_H
+
+#include "src/graphics/ui/menu.c"
+#include "src/graphics/ui/menus/acknowledge_menu.c"
+#include "src/graphics/ui/menus/items_menu.c"
+#include "src/graphics/ui/menus/load_menu.c"
+#include "src/graphics/ui/menus/magic_menu.c"
+#include "src/graphics/ui/menus/party_apply_menu.c"
+#include "src/graphics/ui/menus/party_menu.c"
+#include "src/graphics/ui/menus/party_select_menu.c"
+#include "src/graphics/ui/menus/quit_menu.c"
+#include "src/graphics/ui/menus/fight/action_select_menu.c"
+#include "src/graphics/ui/menus/fight/beast_list_menu.c"
+#include "src/graphics/ui/menus/fight/beast_target_menu.c"
+#include "src/graphics/ui/menus/fight/item_select_menu.c"
+#include "src/graphics/ui/menus/fight/magic_select_menu.c"
+#include "src/graphics/ui/menus/fight/mobile_select_menu.c"
+#include "src/graphics/ui/menus/fight/mobile_target_menu.c"
+
+#endif //CJRPGENGINE_MENUS_H
