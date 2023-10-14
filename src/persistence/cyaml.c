d58478ec8dfaad1a6dfbbc71ec4f09be39f5fe66
Dan Munro <dan@danmunro.com>
2023-10-14 08:29:56 -0700

    reduce duplication here

--- src/persistence/cyaml.c
+++ src/persistence/cyaml.c
@@ -1,18 +1,22 @@
 #include "headers/persistence/cyaml_config.h"
 #include "headers/config.h"
 
+void loadYamlFile(const char *filePath, cyaml_schema_value_t schema, cyaml_data_t **dataOut) {
+    cyaml_err_t err = cyaml_load_file(filePath, &cyamlConfig,
+                                      &schema, dataOut, NULL);
+    if (err != CYAML_OK) {
+        addError("error parsing yaml :: %s, %s",
+                 filePath,
+                 cyaml_strerror(err));
+        exit(CyamlErrorGeneric);
+    }
+}
+
 MusicData *loadMusicYaml(const char *indexDir) {
     MusicData *music = malloc(sizeof(MusicData));
     char filePath[MAX_FS_PATH_LENGTH];
     sprintf(filePath, "%s/audio/music.yaml", indexDir);
-    cyaml_err_t err = cyaml_load_file(filePath, &cyamlConfig,
-                                      &musicTopSchema, (cyaml_data_t **) &music, NULL);
-    if (err != CYAML_OK) {
-        addError("error parsing music yaml :: %s, %s",
-                filePath,
-                cyaml_strerror(err));
-        exit(CyamlErrorGeneric);
-    }
+    loadYamlFile(filePath, musicTopSchema, (cyaml_data_t **) &music);
     return music;
 }
 
@@ -20,26 +24,13 @@ SoundData *loadSoundYaml(const char *indexDir) {
     SoundData *sound = malloc(sizeof(SoundData));
     char filePath[MAX_FS_PATH_LENGTH];
     sprintf(filePath, "%s/audio/sound.yaml", indexDir);
-    cyaml_err_t err = cyaml_load_file(filePath, &cyamlConfig,
-                                      &musicTopSchema, (cyaml_data_t **) &sound, NULL);
-    if (err != CYAML_OK) {
-        addError("error parsing music yaml :: %s, %s",
-                filePath,
-                cyaml_strerror(err));
-        exit(CyamlErrorGeneric);
-    }
+    loadYamlFile(filePath, soundTopSchema, (cyaml_data_t **) &sound);
     return sound;
 }
 
 AnimationData *loadAnimationYaml(const char *filePath) {
     AnimationData *animation = malloc(sizeof(AnimationData));
-    cyaml_err_t err = cyaml_load_file(filePath, &cyamlConfig,
-                                      &animationTopSchema, (cyaml_data_t **) &animation, NULL);
-    if (err != CYAML_OK) {
-        addError("error parsing animation yaml :: %s, %s",
-                filePath, cyaml_strerror(err));
-        exit(CyamlErrorGeneric);
-    }
+    loadYamlFile(filePath, animationTopSchema, (cyaml_data_t **) &animation);
     return animation;
 }
 
@@ -47,93 +38,50 @@ SceneData *loadSceneYaml(const char *indexDir) {
     SceneData *scene = malloc(sizeof(SceneData));
     char filePath[MAX_FS_PATH_LENGTH];
     sprintf(filePath, "%s/scene.yaml", indexDir);
-    cyaml_err_t err = cyaml_load_file(filePath, &cyamlConfig,
-                                      &sceneTopSchema, (cyaml_data_t **) &scene, NULL);
-    if (err != CYAML_OK) {
-        addError("error parsing scene yaml :: %s, %s",
-                filePath, cyaml_strerror(err));
-        exit(CyamlErrorGeneric);
-    }
+    loadYamlFile(filePath, sceneTopSchema, (cyaml_data_t **) &scene);
     return scene;
 }
 
 SpritesheetData *loadSpritesheetYaml(const char *filePath) {
     SpritesheetData *scene = malloc(sizeof(SpritesheetData));
-    cyaml_err_t err = cyaml_load_file(filePath, &cyamlConfig,
-                                      &spritesheetTopSchema, (cyaml_data_t **) &scene, NULL);
-    if (err != CYAML_OK) {
-        addError("error parsing spritesheet yaml :: %s, %s",
-                filePath, cyaml_strerror(err));
-        exit(CyamlErrorGeneric);
-    }
+    loadYamlFile(filePath, spritesheetTopSchema, (cyaml_data_t **) &scene);
     return scene;
 }
 
 MobileData *loadMobYaml(const char *filePath) {
     MobileData *mob = malloc(sizeof(MobileData));
-    cyaml_err_t err = cyaml_load_file(filePath, &cyamlConfig,
-                                      &mobileTopSchema, (cyaml_data_t **) &mob, NULL);
-    if (err != CYAML_OK) {
-        addError("error parsing mob yaml from file :: %s, %s",
-                filePath, cyaml_strerror(err));
-        exit(CyamlErrorGeneric);
-    }
+    loadYamlFile(filePath, mobileTopSchema, (cyaml_data_t **) &mob);
     return mob;
 }
 
 ItemsData *loadItemYaml(const char *filePath) {
     ItemsData *items = malloc(sizeof(ItemsData));
-    cyaml_err_t err = cyaml_load_file(filePath, &cyamlConfig,
-                                      &itemsSchema, (cyaml_data_t **) &items, NULL);
-    if (err != CYAML_OK) {
-        addError("error parsing item yaml :: %s, %s",
-                filePath, cyaml_strerror(err));
-        exit(CyamlErrorGeneric);
-    }
+    loadYamlFile(filePath, itemsSchema, (cyaml_data_t **) &items);
     return items;
 }
 
 BeastiaryData *loadBeastiaryYaml(const char *filePath) {
     BeastiaryData *beastiary = malloc(sizeof(BeastiaryData));
-    cyaml_err_t err = cyaml_load_file(filePath, &cyamlConfig,
-                                      &beastiaryTopSchema, (cyaml_data_t **) &beastiary, NULL);
-    if (err != CYAML_OK) {
-        addError("error parsing beast yaml :: %s, %s",
-                filePath, cyaml_strerror(err));
-        exit(CyamlErrorGeneric);
-    }
+    loadYamlFile(filePath, beastiaryTopSchema, (cyaml_data_t **) &beastiary);
     return beastiary;
 }
 
 StorylinesData *loadStorylinesYaml(const char *filePath) {
     StorylinesData *storylines = malloc(sizeof(StorylinesData));
-    cyaml_err_t err = cyaml_load_file(filePath, &cyamlConfig,
-                                      &storylinesTopSchema, (cyaml_data_t **) &storylines, NULL);
-    if (err != CYAML_OK) {
-        addError("error loading file, filename and error :: %s, %s",
-                filePath, cyaml_strerror(err));
-        exit(CyamlLoadStorylines);
-    }
+    loadYamlFile(filePath, storylinesTopSchema, (cyaml_data_t **) &storylines);
     return storylines;
 }
 
 SaveData *loadSaveData(const char *filePath) {
     SaveData *save = malloc(sizeof(SaveData));
-    cyaml_err_t err = cyaml_load_file(filePath, &cyamlConfig,
-                                      &saveTopSchema, (cyaml_data_t **) &save, NULL);
-    if (err != CYAML_OK) {
-        addError("error loading save data :: %s, %s",
-                filePath,
-                cyaml_strerror(err));
-        exit(CyamlLoadPlayer);
-    }
+    loadYamlFile(filePath, saveTopSchema, (cyaml_data_t **) &save);
     if (save->player->storylines == NULL) {
         save->player->storylines = calloc(MAX_STORIES, sizeof(char *));
     }
     return save;
 }
 
-void saveSaveData(SaveData *saveData, const char *filePath) {
+void saveSaveData(const SaveData *saveData, const char *filePath) {
     addDebug("saving to %s", filePath);
     cyaml_err_t err = cyaml_save_file(filePath, &cyamlConfig,
                                       &saveTopSchema, saveData, 0);
@@ -146,13 +94,7 @@ void saveSaveData(SaveData *saveData, const char *filePath) {
 
 SpellsData *loadSpellData(const char *filePath) {
     SpellsData *spells = malloc(sizeof(SpellsData));
-    cyaml_err_t err = cyaml_load_file(filePath, &cyamlConfig,
-                                      &spellsTopSchema, (cyaml_data_t **) &spells, NULL);
-    if (err != CYAML_OK) {
-        addError("error loading file, filename and error :: %s, %s",
-                filePath, cyaml_strerror(err));
-        exit(CyamlErrorGeneric);
-    }
+    loadYamlFile(filePath, spellsTopSchema, (cyaml_data_t **) &spells);
     return spells;
 }
 
@@ -160,13 +102,7 @@ UIData *loadUIData() {
     char filePath[MAX_FS_PATH_LENGTH];
     sprintf(filePath, "%s/ui.yaml", config->indexDir);
     UIData *ui = malloc(sizeof(UIData));
-    cyaml_err_t err = cyaml_load_file(filePath, &cyamlConfig,
-                                      &uiSchema, (cyaml_data_t **) &ui, NULL);
-    if (err != CYAML_OK) {
-        addError("error loading file, filename and error :: %s, %s",
-                filePath, cyaml_strerror(err));
-        exit(CyamlErrorGeneric);
-    }
+    loadYamlFile(filePath, uiSchema, (cyaml_data_t **) &ui);
     return ui;
 }
 
@@ -174,24 +110,12 @@ StartPartyData *loadStartPartyData() {
     char filePath[MAX_FS_PATH_LENGTH];
     sprintf(filePath, "%s/player/start_party.yaml", config->indexDir);
     StartPartyData *startParty = malloc(sizeof(StartPartyData));
-    cyaml_err_t err = cyaml_load_file(filePath, &cyamlConfig,
-                                      &startPartyTopSchema, (cyaml_data_t **) &startParty, NULL);
-    if (err != CYAML_OK) {
-        addError("error loading file, filename and error :: %s, %s",
-                filePath, cyaml_strerror(err));
-        exit(CyamlErrorGeneric);
-    }
+    loadYamlFile(filePath, startPartyTopSchema, (cyaml_data_t **) &startParty);
     return startParty;
 }
 
 ItemsReferenceData *loadItemsReferenceData(const char *filePath) {
     ItemsReferenceData *items = malloc(sizeof(ItemsReferenceData));
-    cyaml_err_t err = cyaml_load_file(filePath, &cyamlConfig,
-                                      &itemsReferenceSchema, (cyaml_data_t **) &items, NULL);
-    if (err != CYAML_OK) {
-        addError("error parsing item yaml :: %s, %s",
-                filePath, cyaml_strerror(err));
-        exit(CyamlErrorGeneric);
-    }
+    loadYamlFile(filePath, itemsReferenceSchema, (cyaml_data_t **) &items);
     return items;
 }
