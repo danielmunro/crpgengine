57b2bfb1f0b4cdabc866837b1b784c41bcb47f7a
Dan Munro <dan@danmunro.com>
2023-10-29 07:08:22 -0700

    unused

--- src/headers/map.h
+++ src/headers/map.h
@@ -6,10 +6,8 @@
 #define MAX_ARRIVE_AT 64
 #define MAX_MOBILES 256
 #define MAX_MOBILE_MOVEMENTS 64
-#define MAX_TILESETS 256
 #define MAX_TILES 2048
 #define MAX_CHESTS 256
-#define MAX_CHEST_KEY_LENGTH 64
 
 #include "map.c"
 
