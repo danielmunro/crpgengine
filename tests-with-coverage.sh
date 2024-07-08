d001fc59cc54cf48fdb1711bfdeda4e88930c093
Dan Munro <dan@danmunro.com>
2024-07-08 07:26:04 -0700

    coverage

--- tests-with-coverage.sh
+++ tests-with-coverage.sh
@@ -1,2 +1,4 @@
 make
 LLVM_PROFILE_FILE="test.profraw" ./build/tests
+llvm-profdata merge -sparse test.profraw -o test.profdata
+llvm-cov show --instr-profile test.profdata build/tests -use-color --format html src/ > coverage.html
